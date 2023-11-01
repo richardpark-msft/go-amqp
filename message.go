d02ccabd8724db9533464cc6753716cdb127753f
Richard Park <ripark@microsoft.com>
2023-11-01 08:16:29 -0700

    Constrain the type - it's just octets.

--- message.go
+++ message.go
@@ -103,7 +103,7 @@ type Message struct {
 
 	// TransactionID can be used by senders to ensure a message is included as part of a transaction, started with
 	// [TransactionController.Declare].
-	TransactionID any
+	TransactionID []byte
 
 	deliveryID uint32 // used when sending disposition
 	settled    bool   // whether transfer was settled by sender
